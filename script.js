82a60b169f42451bd9474341905d8ada774c9a59
Martina <martina.pozzerle@libero.it>
2020-10-22 16:06:29 +0200

    #email finito

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 //esercizio email
-//creo var con inserimento mail
+//creo array con mail possibili
 var emails = ["visitatore@gmail.it","visitatore@gmail.com","visitatore@libero.it","visitatore@alice.it","visitatore@hotmail.it"];
+//creo input per inserimento mail
 var inputEmail = prompt("Ciao visitatore! Inserisci la tua mail");
-var message = "Mi dispiace, non abbiamo trovato questa mail";
+//creo una var message da inserire poi nell'html
+var message = "Mi dispiace, non abbiamo trovato la tua mail";
+//creo ciclo con condizione per verificare la mail inserita
 for (var i = 0; i < emails.length; i++){
   if (emails[i] == inputEmail){
-    message = "Benvenuto visitatore!";
+    message = "Benvenuto!";
+    console.log("Benvenuto visitatore!");
+
+  } else{
+    message;
+    console.log("Mi dispiace, non abbiamo trovato la tua mail");
   }
 }
-
-console.log(message);
+//creo messaggi di output
+alert(message);
